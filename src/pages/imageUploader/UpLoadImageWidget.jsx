1e6f28c4703f2115bf0923917411e304efe21e43
LordNedStark <osisiogubenjamin1@gmail.com>
2023-10-20 05:24:53 +0100

    rectifying failed deployment

--- src/pages/imageUploader/UpLoadImageWidget.jsx
+++ src/pages/imageUploader/UpLoadImageWidget.jsx
@@ -1,35 +1,35 @@
-import {useEffect, useRef} from "react";
-import { get_CLOUDINARY_API_NAME, get_CLOUDINARY_UPLOAD_PRESET } from "../../../securedFiles/.CloudinarySecrets";
-// require('dotenv').config();
+// import {useEffect, useRef} from "react";
+// import { get_CLOUDINARY_API_NAME, get_CLOUDINARY_UPLOAD_PRESET } from "../../../securedFiles/.CloudinarySecrets";
+// // require('dotenv').config();
 
 
-function UploadWidget() {
+// function UploadWidget() {
     
-    const cloudinaryRef = useRef()
-    const widgetRef = useRef()
-    useEffect(()=>{
-        cloudinaryRef.current = window.cloudinary
-        console.log(cloudinaryRef.current)
-        widgetRef.current = cloudinaryRef.current.createUploadWidget({
+//     const cloudinaryRef = useRef()
+//     const widgetRef = useRef()
+//     useEffect(()=>{
+//         cloudinaryRef.current = window.cloudinary
+//         console.log(cloudinaryRef.current)
+//         widgetRef.current = cloudinaryRef.current.createUploadWidget({
             
-            cloudName:  get_CLOUDINARY_API_NAME(),
-            uploadPreset: get_CLOUDINARY_UPLOAD_PRESET()
-            // uploadPreset: process.env.CLOUDINARY_UPLOAD_PRESET
-        },  function (error, result) {
-            if (!error && result && result.event === 'success') {
+//             cloudName:  get_CLOUDINARY_API_NAME(),
+//             uploadPreset: get_CLOUDINARY_UPLOAD_PRESET()
+//             // uploadPreset: process.env.CLOUDINARY_UPLOAD_PRESET
+//         },  function (error, result) {
+//             if (!error && result && result.event === 'success') {
 
-                sessionStorage.setItem("recentlyUploadedImage",result.info.secure_url )
+//                 sessionStorage.setItem("recentlyUploadedImage",result.info.secure_url )
                
-                return result.info.secure_url;
-            }
-            else alert(error)
-        })
-    }, [])
+//                 return result.info.secure_url;
+//             }
+//             else alert(error)
+//         })
+//     }, [])
 
-    return (
-        <button onClick={() =>widgetRef.current.open()}>
-            Upload Product Image 
-        </button>
-    )
-}
-export default UploadWidget
+//     return (
+//         <button onClick={() =>widgetRef.current.open()}>
+//             Upload Product Image 
+//         </button>
+//     )
+// }
+// export default UploadWidget
